3346cc667e705e6dbe8910f7cb461e5cb924adef
Janos Juhasz <janos.juhasz@hu.ibm.com>
2016-03-27 12:45:06 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+ $world = "world!";
+ echo "<p>Hello $world</p>"; 
+ $world = "more text";
+  echo "<p>Hello $world</p>"; 
+ ?> 
+ </body>
+</html>
